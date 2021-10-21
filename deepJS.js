bc5e5edb691412b830e94825ee4c41a0e09a52ee
Natali-Nazarenko <91319785+Natali-Nazarenko@users.noreply.github.com>
2021-10-21 15:07:24 +0300

    Natali. fix comment

--- deepJS.js
+++ deepJS.js
@@ -1,6 +1,6 @@
 "use strict"
 
-//#1 Написать свою реализацию функций bind, call.
+ //#1 Написать свою реализацию функций bind, call.
 let user = {
     name: 'Vasya',
     age: 23,
@@ -19,40 +19,37 @@ let user3 = {
 function userData(phone, city) {
     console.log(`Name: ${this.name}, Age: ${this.age}, Tel: ${phone}, City: ${city}`);
 }
-
-Function.prototype.my_bind = function (context, ...args) {
+Function.prototype.myBind = function (context, ...args) {
     return (...rest) => {
         let bufferObj = Object.create(context);
         bufferObj.func = this;
-        console.log('bufferObj.fn; ', bufferObj.func)
-        const result = bufferObj.func(...args, ...rest);
-        bufferObj = null;
+        let result = bufferObj.func(...args, ...rest);
         return result;
     }
 }
-const foo = userData.my_bind(user, '111111111', 'Kharkiv')();
+const foo = userData.myBind(user, '11117711111', 'Kharkiv')();
 
-Function.prototype.my_call = function (context, ...args) {
+Function.prototype.myCall = function (context, ...args) {
     let bufferObj = Object.create(context);
     bufferObj.func = this;
-    bufferObj.func(...args);
-    bufferObj = null;
+    let result = bufferObj.func(...args);
+    return result;
 }
-const foo2 = userData.my_call(user2, '222222222', 'Kiev');
+const foo2 = userData.myCall(user2, '222222222', 'Kiev');
 
-Function.prototype.my_apply = function (context, argsArray) {
+Function.prototype.myApply = function (context, argsArray) {
     let bufferObj = Object.create(context);
     bufferObj.func = this;
-    bufferObj.func(...argsArray);
-    bufferObj = null;
+    let result = bufferObj.func(...argsArray);
+    return result;
 }
-const foo3 = userData.my_apply(user3, ['333333333', 'Lviv']);
+const foo3 = userData.myApply(user3, ['333333333', 'Lviv']);
 
 
 //#2 Написать свою реализацию функций map, filter, reduce, forEach.
 const arr = [1, 2, 3, 4, 5, 7, 8];
 
-Array.prototype.my_map = function (callback) {
+Array.prototype.myMap = function (callback) {
     const resultArrayElem = [];
     for (let i = 0; i < this.length; i++) {
         resultArrayElem[i] = callback(this[i], i, this);
@@ -60,7 +57,7 @@ Array.prototype.my_map = function (callback) {
     return resultArrayElem;
 };
 
-Array.prototype.my_filter = function (callback) {
+Array.prototype.myFilter = function (callback) {
     const resultArrayElem = [];
     for (let i = 0; i < this.length; i++) {
         const isTrueElem = callback(this[i], i, this);
@@ -71,7 +68,7 @@ Array.prototype.my_filter = function (callback) {
     return resultArrayElem;
 };
 
-Array.prototype.my_reduce = function (callback, initialValue) {
+Array.prototype.myReduce = function (callback, initialValue) {
     let result;
     let i = 0;
 
@@ -87,7 +84,8 @@ Array.prototype.my_reduce = function (callback, initialValue) {
     return result;
 };
 
-Array.prototype.my_forEach = function (callback) {
+
+Array.prototype.myForEach = function (callback) {
     for (let i = 0; i < this.length; i++) {
         callback(this[i], i, this);
     }
