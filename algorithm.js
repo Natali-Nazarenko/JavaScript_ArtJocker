5be0c21a6981ea1c96eb5646ad4afe131e533445
Natali-Nazarenko <91319785+Natali-Nazarenko@users.noreply.github.com>
2021-11-05 10:38:57 +0200

    last correction binary tree

--- algorithm.js
+++ algorithm.js
@@ -1,149 +1,191 @@
 //#1 Написать свою реализацию бинарного дерева поиска. 
-//(Возможности структуры данных должны быть: Добавить новый элемент, удалить элемент, найти элемент по его значению)
-
-class Node {
-    constructor(data) {
-        this.value = data;
-        this.left = null;
-        this.right = null;
-        this.key = String(Date.now()) + Math.floor(Math.random() * 10000);
-        this.parent = '';
-    }
-};
 
 class BinaryTree {
     constructor() {
         this.root = null;
-    };
+    }
 
-    entry(data) {
+    createNewNode(data) {
         // debugger
-        let newNode = new Node(data);
+        let newNode = {};
 
         if (this.root === null) {
-            this.root = newNode;
-            return this.root;
-        }
-        return this.addNode(this.root, newNode);
-    };
+            this.root = {
+                value: data,
+                left: null,
+                right: null,
+            }
 
-    addNode(node, newNode) {//добавление нового узла
+            return this.root;
+        } else {
+            newNode = {
+                value: data,
+                left: null,
+                right: null,
+            }
+        }
+
+        return this.add(this.root, newNode);
+    }
+
+    add(node, newNode) {//добавление нового узла
 
         if (node.value > newNode.value) {
 
             if (node.left === null) {
                 node.left = newNode;
-                newNode.parent = `${node.key}L`;
             } else {
-                return this.addNode(node.left, newNode);
+
+                return this.add(node.left, newNode);
             }
         } else {
 
             if (node.right === null) {
                 node.right = newNode;
-                newNode.parent = `${node.key}R`;
-            } else {
-                return this.addNode(node.right, newNode);
             }
-        }
-        return node;
-    };
 
-    searchNode(element, node = this.root) {//поиск заданного узла
+            return this.add(node.right, newNode);
+        }
+
+        return node;
+    }
+
+    search(element, node) {//поиск заданного узла
+        // debugger
+
+        if (node === null) {
+            return null;
+        }
+
+        node = node || this.root;
 
         if (element === node.value) {
             return node;
         } else if (element < node.value) {
-            return this.searchNode(element, node.left);
+            return this.search(element, node.left);
         } else if (element > node.value) {
-            return this.searchNode(element, node.right);
+            return this.search(element, node.right);
         }
-        return node;
-    };
 
-    findParent(element, key, node = this.root) {//поиск родителя, удаляемого узла
-        // debugger
-        if (key == node.key) {
-            return node;
-        } else if (element < node.value) {
-            return this.findParent(element, key, node.left);
-        } else if (element > node.value) {
-            return this.findParent(element, key, node.right);
-        }
         return node;
-    };
+    }
+
+    findParent(element, nodeParent) {//поиск родителя, удаляемого узла
+        //debugger
+        nodeParent = nodeParent || this.root;
+
+        if (element === nodeParent.value) {
+
+            return nodeParent;
+        } else if (nodeParent.left != null && nodeParent.right != null) {
+
+            if ((element === nodeParent.right.value) || (element === nodeParent.left.value)) {
+
+                return nodeParent;
+            } else if (element < nodeParent.value) {
+
+                return this.findParent(element, nodeParent.left);
+            } else if (element > nodeParent.value) {
+
+                return this.findParent(element, nodeParent.right);
+            }
+        } else if (nodeParent.left === null && element != nodeParent.right) {
+
+            if (element === nodeParent.right.value) {
+
+                return nodeParent;
+            } else if (element < nodeParent.value) {
+
+                return this.findParent(element, nodeParent.left);
+            } else if (element > nodeParent.value) {
+
+                return this.findParent(element, nodeParent.right);
+            }
+        } else if (nodeParent.right === null && element != nodeParent.left) {
+
+            if (element === nodeParent.left.value) {
+
+                return nodeParent;
+            } else if (element < nodeParent.value) {
+
+                return this.findParent(element, nodeParent.left);
+            } else if (element > nodeParent.value) {
+
+                return this.findParent(element, nodeParent.right);
+            }
+        }
+
+        return nodeParent;
+    }
 
     findMinimalNode(node) {//поиск минимального узла в левых ветках
         // debugger
         if (node.left === null) {
             return node;
         }
-        return this.findMinimalNode(node.left);
-    };
 
-    deleteNode(element, startingNode) {
+        return this.findMinimalNode(node.left);
+    }
+
+    delete(element) {
         // debugger
-        let node = this.searchNode(element, startingNode);
-        let letter = node.parent[node.parent.length - 1];
-        let keyParent = node.parent.slice(0, -1);
+        let nodeParent = this.findParent(element);
+        let node = this.search(element);
 
         //удаление узла без потомков
         if (node.left === null && node.right === null) {
-            node = null;
-            return node;
+
+            if (nodeParent.left === null && nodeParent.right.value === node.value) {
+                nodeParent.right = null;
+
+                return nodeParent;
+            }
+            nodeParent.left = null;
+
+            return nodeParent;
         }
 
         //удаление узла с одним потомком
         if (node.left === null) {
-            node = node.right;
-            return node;
-        } else if (node.right === null) {
-            node = node.left;
-            return node;
+
+            if (nodeParent.value > node.value) {
+                nodeParent.left = node.right;
+
+                return nodeParent;
+            }
+            nodeParent.right = node.right;
+
+            return nodeParent;
+        }
+        
+        if (node.right === null) {
+
+            if (nodeParent.value > node.value) {
+                nodeParent.left = node.left;
+
+                return nodeParent;
+            }
+            nodeParent.right = node.left;
+
+            return nodeParent;
         }
 
         //удаление узла с двумя потомками
-        let parentNode = this.findParent(element, keyParent);
-        let newNode = this.findMinimalNode(node.right);
-        let cloneNewNode = new Node(newNode.value);
-        cloneNewNode = Object.assign(cloneNewNode, newNode);
+        let minNode = this.findMinimalNode(node.right);
+        this.delete(minNode.value);
+        node.value = minNode.value;
 
-        if (letter === 'R') {
-            parentNode.right = cloneNewNode;
-            cloneNewNode.left = node.left;
-            cloneNewNode.parent = `${parentNode.key}R`;
+        return node;
+    }
 
-            if (node.right.value != newNode.value) {
-                cloneNewNode.right = node.right;
-                node.right.left = newNode.right;
-            } else {
-                parentNode.right.right = newNode.right;
-            }
-            return parentNode;
-        }
-
-        if (letter === 'L') {
-            parentNode.left = cloneNewNode;
-            cloneNewNode.left = node.left;
-            cloneNewNode.parent = `${parentNode.key}L`;
-
-            if (node.right.value != newNode.value) {
-                cloneNewNode.right = node.right;
-                node.right.left = newNode.right;
-            } else {
-                parentNode.left.right = newNode.right;
-            }
-            return parentNode;
-        }
-    };
-};
+}
 
 const arrayForNode = [33, 1, 4, 9, 7, 44, 706, 88, 22, 65, 8, 45, 710, 3, 2, 10, 99, 27, 17, 100, -2];
 
 const binaryTree = new BinaryTree();
 
 for (let i = 0; i < arrayForNode.length; i++) {
-    binaryTree.entry(arrayForNode[i]);
+    binaryTree.createNewNode(arrayForNode[i]);
 };
 
 //#2 Написать сортировку двумя различными методами
@@ -151,33 +193,33 @@ for (let i = 0; i < arrayForNode.length; i++) {
 //Сортировка вставками
 
 Array.prototype.insertSort = function (callback) {
-    let arraySort = this;
-    for (let i = 0; i < arraySort.length; i++) {
-        let buf = arraySort[i];
+
+    for (let i = 0; i < this.length; i++) {
+        let buf = this[i];
         let j = i - 1;
-        while (j >= 0 && callback(arraySort[j], buf)) {
-            arraySort[j + 1] = arraySort[j];
+        while (j >= 0 && callback(this[j], buf)) {
+            this[j + 1] = this[j];
             j--;
         }
-        arraySort[j + 1] = buf;
+        this[j + 1] = buf;
     }
-    return arraySort;
+    return this;
 }
 
 //сортировка выбором
 
 Array.prototype.selectionSort = function (callback) {
-    let arraySort = this;
-    for (let i = 0; i < arraySort.length - 1; i++) {
+
+    for (let i = 0; i < this.length - 1; i++) {
         let min = i;
-        for (let j = i + 1; j < arraySort.length; j++) {
-            if (callback(arraySort[j], arraySort[min])) {
+        for (let j = i + 1; j < this.length; j++) {
+            if (callback(this[j], this[min])) {
                 min = j;
             }
         }
-        let buf = arraySort[min];
-        arraySort[min] = arraySort[i];
-        arraySort[i] = buf;
+        let buf = this[min];
+        this[min] = this[i];
+        this[i] = buf;
     }
-    return arraySort;
+    return this;
 }
